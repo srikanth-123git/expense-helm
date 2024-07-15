1fe0c75be17d085f16a64289aa7716009d0e1942
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-15 22:05:41 +0530

    ArgoCd

--- argo.sh
+++ argo.sh
@@ -2,8 +2,3 @@ kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
-sleep 10
-
-echo URL - https://$(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1)
-echo Username - admin
-echo Password - $(argocd admin initial-password -n argocd | head -1)
