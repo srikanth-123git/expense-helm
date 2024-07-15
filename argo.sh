48d901ef46dfd3ca2afd77c62e09654c7931e560
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-15 21:58:44 +0530

    ArgoCd

--- argo.sh
+++ argo.sh
@@ -1,21 +1,9 @@
-if [ "$1" == "install" ]; then
-  kubectl create namespace argocd
-  kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-  kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
-  sleep 10
+sleep 10
 
-  echo URL - https://$(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1)
-  echo Username - admin
-  echo Password - $(argocd admin initial-password -n argocd | head -1)
-
-fi
-
-if [ "$1" == "jobs" ]; then
-  argocd login $(kubectl get svc -n argocd argocd-server | awk '{print $4}') --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
-
-  for app in backend frontend ; do
-     argocd app create ${app}  --repo https://github.com/srikanth-123git/expense-helm --path chart --dest-namespace default --dest-server https://kubernetes.default.svc --grpc-web --values values/${app}.yaml
-  done
-
-fi
+echo URL - https://$(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1)
+echo Username - admin
+echo Password - $(argocd admin initial-password -n argocd | head -1)
