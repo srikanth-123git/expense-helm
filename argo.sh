a704a08f8328288c0b11dfc5918065ed2670a64f
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-17 18:50:55 +0530

    Argocd

--- argo.sh
+++ argo.sh
@@ -1,11 +1,17 @@
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+if [ "$1" == "install" ]; then
+  kubectl create namespace argocd
+  kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+  kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
-sleep 10
+  sleep 10
 
-echo URL - https://$(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1)
-echo Username - admin
-echo Password - $(argocd admin initial-password -n argocd | head -1)
+  echo URL - https://$(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1)
+  echo Username - admin
+  echo Password - $(argocd admin initial-password -n argocd | head -1)
 
+fi
 
+if [ "$1" == "jobs" ]; then
+  argocd login $(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1) --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
+
+fi
