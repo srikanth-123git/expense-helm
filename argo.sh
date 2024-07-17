03f3e0e50e3d8c6e9c70341a9dba8f553e293f7b
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-17 18:55:14 +0530

    Argocd

--- argo.sh
+++ argo.sh
@@ -14,4 +14,9 @@ fi
 if [ "$1" == "jobs" ]; then
   argocd login $(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1) --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
 
+  for app in backend frontend ; do
+     argocd app create ${app}  --repo https://github.com/srikanth-123git/expense-helm --path chart --dest-namespace default --dest-server https://kubernetes.default.svc --grpc-web --values values/${app}.yaml
+  done
+
 fi
+
