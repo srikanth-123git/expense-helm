fd7392afab2178df5bec24b7c9c0042570aa9233
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-20 22:06:44 +0530

    Argo Yaml

--- infra-helm-charts/install.sh
+++ infra-helm-charts/install.sh
@@ -1,3 +1,3 @@
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm upgrade -i ngx-ingres ingress-nginx/ingress-nginx -f ingress.yaml
-helm install -f argo-dev.yaml
+kubectl apply -f argo-dev.yaml
